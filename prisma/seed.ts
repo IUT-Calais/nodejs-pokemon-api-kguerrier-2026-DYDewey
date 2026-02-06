60436ed740c612d4f10deb0d77e8e878c8bd7749
DYDewey <vverstaevel15@gmail.com>
2026-02-06 13:57:25 +0100

    Change pokemon type system

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -31,181 +31,224 @@ async function main() {
   });
 
 
-await prisma.pokemonCard.createMany({
-  data: [
-    {
+const getTypeId = async (name: string) => {
+  const t = await prisma.type.findUnique({ where: { name } });
+  if (!t) {
+    throw new Error(`Type not found: ${name}`);
+  }
+  return t.id;
+};
+
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Bulbizarre',
       pokedexId: 1,
-      typeId: 4, 
+      typeId: await getTypeId('Grass'),
       lifePoints: 45,
       size: 0.7,
       weight: 6.9,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Salamèche',
       pokedexId: 4,
-      typeId: 2,
+      typeId: await getTypeId('Fire'),
       lifePoints: 39,
       size: 0.6,
       weight: 8.5,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Carapuce',
       pokedexId: 7,
-      typeId: 3,
+      typeId: await getTypeId('Water'),
       lifePoints: 44,
       size: 0.5,
       weight: 9.0,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Pikachu',
       pokedexId: 25,
-      typeId: 5,
+      typeId: await getTypeId('Electric'),
       lifePoints: 35,
       size: 0.4,
       weight: 6.0,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
     },
-    {
-      name: 'Lipoutou', 
+  });
+  await prisma.pokemonCard.create({
+    data: {
+      name: 'Lipoutou',
       pokedexId: 124,
-      typeId: 6, 
+      typeId: await getTypeId('Ice'),
       lifePoints: 65,
       size: 1.4,
       weight: 40.6,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/124.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Machopeur',
       pokedexId: 67,
-      typeId: 7,
+      typeId: await getTypeId('Fighting'),
       lifePoints: 80,
       size: 1.5,
       weight: 70.5,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/067.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Smogogo',
       pokedexId: 110,
-      typeId: 8, 
+      typeId: await getTypeId('Poison'),
       lifePoints: 65,
       size: 1.2,
       weight: 9.5,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/110.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Taupiqueur',
       pokedexId: 50,
-      typeId: 9, 
+      typeId: await getTypeId('Ground'),
       lifePoints: 10,
       size: 0.2,
       weight: 0.8,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Roucool',
       pokedexId: 16,
-      typeId: 10, 
+      typeId: await getTypeId('Flying'),
       lifePoints: 40,
       size: 0.3,
       weight: 1.8,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Abra',
       pokedexId: 63,
-      typeId: 11, 
+      typeId: await getTypeId('Psychic'),
       lifePoints: 25,
       size: 0.9,
       weight: 19.5,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/063.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Aspicot',
       pokedexId: 13,
-      typeId: 12,
+      typeId: await getTypeId('Bug'),
       lifePoints: 40,
       size: 0.3,
       weight: 3.2,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Racaillou',
       pokedexId: 74,
-      typeId: 13,
+      typeId: await getTypeId('Rock'),
       lifePoints: 40,
       size: 0.4,
       weight: 20.0,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/074.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Fantominus',
       pokedexId: 92,
-      typeId: 14,
+      typeId: await getTypeId('Ghost'),
       lifePoints: 30,
       size: 1.3,
       weight: 0.1,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/092.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Minidraco',
       pokedexId: 147,
-      typeId: 15, 
+      typeId: await getTypeId('Dragon'),
       lifePoints: 41,
       size: 1.8,
       weight: 3.3,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/147.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Noctali',
       pokedexId: 197,
-      typeId: 16,
+      typeId: await getTypeId('Dark'),
       lifePoints: 95,
       size: 1.0,
       weight: 27.0,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Magnéti',
       pokedexId: 81,
-      typeId: 17,
+      typeId: await getTypeId('Steel'),
       lifePoints: 25,
       size: 0.3,
       weight: 6.0,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/081.png',
     },
-    {
+  });
+  await prisma.pokemonCard.create({
+    data: {
       name: 'Togepi',
       pokedexId: 175,
-      typeId: 18, 
+      typeId: await getTypeId('Fairy'),
       lifePoints: 35,
       size: 0.3,
       weight: 1.5,
       imageUrl:
         'https://assets.pokemon.com/assets/cms2/img/pokedex/full/175.png',
     },
-  ],
-});
-
+  });
+  await prisma.user.create({
+    data: {
+      email: 'admin@gmail.com',
+      password: 'admin',
+    },
+  });
 }
 
 main()
@@ -216,4 +259,3 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
-
