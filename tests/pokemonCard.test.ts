a3d1b399405a10af6d0892421fdfd2c59f93c9de
DYDewey <vverstaevel15@gmail.com>
2026-03-21 13:46:12 +0100

    Add Test PATCH

--- tests/pokemonCard.test.ts
+++ tests/pokemonCard.test.ts
@@ -22,3 +22,37 @@ describe('GET /pokemon-cards', () => {
     expect(response.body).toEqual(pokemonsMock);
   });
 });
+
+  describe('PATCH /pokemon-cards/:pokemonCardId', () => {
+    it('devrait modifier un pokemon existant avec un token valide (Status 200)', async () => {
+      const existingCard = {
+        id: 1, name: 'Bulbizarre', pokedexId: 1, typeId: 1, lifePoints: 45
+      };
+      const updatedCard = { ...existingCard, lifePoints: 50 };
+
+      // Prisma trouve la carte existante
+      prismaMock.pokemonCard.findUnique.mockResolvedValueOnce(existingCard as any);
+      
+      // Prisma simule la mise à jour
+      prismaMock.pokemonCard.update.mockResolvedValue(updatedCard as any);
+
+      const response = await request(app)
+        .patch('/pokemon-cards/1')
+        .set('Authorization', 'Bearer mockedToken') // Token requis !
+        .send({ lifePoints: 50 }); // On modifie juste les points de vie
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(updatedCard);
+    });
+
+    it('devrait retourner 404 si on modifie un pokemon inexistant', async () => {
+      prismaMock.pokemonCard.findUnique.mockResolvedValue(null);
+
+      const response = await request(app)
+        .patch('/pokemon-cards/999')
+        .set('Authorization', 'Bearer mockedToken')
+        .send({ lifePoints: 50 });
+
+      expect(response.status).toBe(404);
+    });
+  });
